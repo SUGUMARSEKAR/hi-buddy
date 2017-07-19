c4f2180d901d22bcdfbd07ecd1b9bd5e9c226fe1
SUGUMAR SEKAR <majesticsugu@gmail.com>
2017-07-19 16:59:04 +0530

    Update delKdigits.java

--- delKdigits.java
+++ delKdigits.java
@@ -1,20 +1,21 @@
 import java.io.*;
 import java.util.*;
-public class Git
+public class Main
 {
 public static void main(String args[])throws IOException
 {
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-ArrayList<String> arr = new ArrayList<String>();
 String x = br.readLine();
 Integer k = Integer.parseInt(br.readLine());
-Integer cnt = x.length()-k;
-for(int i=0;i<x.length()-cnt;i++)
-{
-arr.add(x.substring(i,cnt));
-cnt++;
-}
-System.out.println(arr);
-System.out.println("Minimum after deleting "+k+" digits "+Collections.min(arr));
+Integer n=Integer.parseInt(x);
+int l=x.length();
+int a[]=new int[l];
+for(int i=0;i<l;i++){
+	a[i]=n%10;
+	n=n/10;
+	}
+Arrays.sort(a);
+for(int i=0;i<l-k;i++)
+System.out.print(a[i]);
 }
 }
